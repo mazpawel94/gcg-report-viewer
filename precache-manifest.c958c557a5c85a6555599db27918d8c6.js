a403fc694cb3bb23201653ccb0f4c37154789bcf
mazpawel94 <mazpawel94@gmail.com>
2020-04-22 21:19:35 +0200

    Updates

---
+++ precache-manifest.c958c557a5c85a6555599db27918d8c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4beaecca1259af120f0cdca6858ea7c",
+    "url": "/gcg-report-viewer/index.html"
+  },
+  {
+    "revision": "9c85ac5b14f69876839a",
+    "url": "/gcg-report-viewer/static/css/main.9c088d7b.chunk.css"
+  },
+  {
+    "revision": "9089fd6548356925afff",
+    "url": "/gcg-report-viewer/static/js/2.2141c387.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gcg-report-viewer/static/js/2.2141c387.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c85ac5b14f69876839a",
+    "url": "/gcg-report-viewer/static/js/main.770313f8.chunk.js"
+  },
+  {
+    "revision": "9fe0362a8df105d035b5",
+    "url": "/gcg-report-viewer/static/js/runtime-main.60de8969.js"
+  }
+]);
