db9238b9e4aa6209bed3bd561af8d911d5b8e1b7
mazpawel94 <mazpawel94@gmail.com>
2020-05-20 19:25:07 +0200

    add special class to no-played tile

--- src/components/Rack/Rack.js
+++ src/components/Rack/Rack.js
@@ -2,10 +2,22 @@ import React from 'react';
 import Tile from '../Tile/Tile'
 
 const Rack = ({ move, actualOption }) => {
-    // const { freeLetters } = move.choiceOptions[actualOption];
+
+    const freeLetters = move.choiceOptions[actualOption].freeLetters.split('');
+
+    const checkAndRemoveLetter = el => {
+        const index = freeLetters.indexOf(el);
+        if (index !== -1) {
+            freeLetters.splice(index, 1);
+            return false;
+        }
+        return true
+    }
+
     const lettersDivs = move.letters.split('').map(el => (
-        <Tile letter={el} />
+        <Tile letter={el} played={checkAndRemoveLetter(el)} />
     ))
+
     return (
         <div>
             {lettersDivs}
--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -3,9 +3,9 @@ import styles from './Tile.module.scss'
 
 import { POINTS } from '../globalVariables'
 
-const Tile = ({ letter }) => {
+const Tile = ({ letter, played }) => {
     return (
-        <div className={styles.tile}>
+        <div className={played ? styles.tilePlayed : styles.tile}>
             {letter}
             <sub className={styles.points}>{POINTS[letter]}</sub>
         </div>
