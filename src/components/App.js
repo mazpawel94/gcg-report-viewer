aca216d386b3fb9ea8930e23edaa28e900dbfd7a
mazpawel94 <mazpawel94@gmail.com>
2020-05-23 17:21:47 +0200

    add possibility change option and move by keydown

--- src/components/App.js
+++ src/components/App.js
@@ -26,6 +26,33 @@ class App extends Component {
         if (this.state.actualMove !== 0)
             this.setState({ actualMove: this.state.actualMove - 1 });
     };
+
+    setNextOption = () => {
+        const { actualOption, actualMove, moves } = this.state;
+        if (actualOption + 1 < moves[actualMove].choiceOptions.length)
+            this.setState({ actualOption: actualOption + 1 });
+    };
+
+    setPreviousOption = () => {
+        if (this.state.actualOption !== 0)
+            this.setState({ actualOption: this.state.actualOption - 1 });
+    };
+
+    handleKeyDown = e => {
+        e.preventDefault();
+        if (e.keyCode === 39)
+            this.setNextMove();
+        if (e.keyCode === 37)
+            this.setPreviousMove();
+        if (e.keyCode === 38)
+            this.setPreviousOption();
+        if (e.keyCode === 40)
+            this.setNextOption();
+    }
+
+    componentDidMount() {
+        document.addEventListener("keydown", this.handleKeyDown);
+    }
     render() {
         const { actualMove, moves, actualOption } = this.state;
         return (
