acc81b899e60466007af1e49fce470617f1a89ca
mazpawel94 <mazpawel94@gmail.com>
2020-11-12 21:18:31 +0100

    add coordinates and needed part fields on board

--- src/components/organisms/Board.js
+++ src/components/organisms/Board.js
@@ -3,7 +3,7 @@ import styled, { css } from "styled-components";
 import { Stage, Layer, Rect } from "react-konva";
 import Word from "./Word";
 import BoardField from "../atoms/BoardField";
-import Rack3d from '../molecules/Rack3d';
+import Rack3d from "../molecules/Rack3d";
 import context from "../../context";
 import { getWords, isMoveWithWord } from "../../services/gameService";
 import {
@@ -20,14 +20,51 @@ const StyledWrapper = styled.div`
   width: 650px;
   background-color: #08763b;
   border: 3px solid rgb(34, 51, 51);
-    transform: translateX(65%) perspective(1000px) rotateX(70deg) rotateZ(-45deg);
+  transform: translateX(65%) perspective(1000px) rotateX(70deg) rotateZ(-45deg);
   transform-style: preserve-3d;
   transition: 1s linear;
   -webkit-transition: 1s linear;
 
-  ${({ perspective }) => perspective && css`
-    transform: rotateX(0);
-  `}
+  ${({ perspective }) =>
+    perspective &&
+    css`
+      transform: rotateX(0);
+    `}
+`;
+
+const UpCoordinates = styled.div`
+  position: absolute;
+  color: white;
+  line-height: 30px;
+  left: 40px;
+  height: 30px;
+  width: calc(100% - 80px);
+  font-size: 11px;
+  display: flex;
+`;
+
+const LeftCoordinates = styled.div`
+  position: absolute;
+  color: white;
+  top: 30px;
+  left: 10px;
+  height: calc(100% - 80px);
+  width: 30px;
+  font-size: 11px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const CoordinateX = styled.div`
+  width: calc(100% / 15);
+  text-align: center;
+`;
+
+const CoordinateY = styled.div`
+  height: calc(100% / 15);
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const GameArea = styled.div`
@@ -37,7 +74,32 @@ const GameArea = styled.div`
   height: calc(100% - 80px);
   width: calc(100% - 80px);
   z-index: 10;
- 
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    background-color: #c02929;
+    width: calc(100% / 15);
+    height: calc(100% / 15);
+    transform: rotate(45deg);
+    left: 30x;
+    box-shadow: 80px 80px #7590c7, 188px 188px #c02929, 294px 294px #7590c7,
+      376px 376px #c02929, 80px -80px #7590c7, 188px -188px #c02929,
+      296px -296px #7590c7, 376px -376px #c02929, 456px -297px #7590c7,
+      564px -189px #c02929, 673px -80px #7590c7, 752px 0px #c02929,
+      673px 80px #7590c7, 564px 189px #c02929, 456px 297px #7590c7;
+  }
+  &::after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    box-shadow: 0px 0px 0px 1px #badce9;
+  }
 `;
 
 const BonusFields = (coords, type) =>
@@ -58,13 +120,22 @@ const Board = () => {
     actualMoveIndex !== undefined &&
     moves[actualMoveIndex].choiceOptions[actualOptionIndex];
 
-
-
   return (
     <>
       <Rack3d />
       <Rack3d top />
       <StyledWrapper perspective={!!actualOption}>
+        {/* <StyledWrapper perspective={true}> */}
+        <UpCoordinates>
+          {[...Array(15).keys()].map((el) => (
+            <CoordinateX>{el + 1}</CoordinateX>
+          ))}
+        </UpCoordinates>
+        <LeftCoordinates>
+          {"ABCDEFGHIJKLMNO".split("").map((el) => (
+            <CoordinateY>{el}</CoordinateY>
+          ))}
+        </LeftCoordinates>
         <GameArea>
           <Stage width={570} height={570}>
             <Layer>
@@ -80,12 +151,13 @@ const Board = () => {
             </Layer>
           </Stage>
           {actualOption && getWords(moves, actualMoveIndex)}
-          {actualOption && isMoveWithWord(actualOption) &&
+          {actualOption && isMoveWithWord(actualOption) && (
             <Word
               letters={actualOption.word}
               coordinates={actualOption.coordinates}
               actualMoveIndex
-            />}
+            />
+          )}
         </GameArea>
       </StyledWrapper>
     </>
