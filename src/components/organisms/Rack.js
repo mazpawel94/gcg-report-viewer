d410d822ab6f236976c5d5ad1b4589540d091d62
mazpawel94 <mazpawel94@gmail.com>
2021-01-15 21:33:34 +0100

    add tests for Rack

--- src/components/organisms/Rack.js
+++ src/components/organisms/Rack.js
@@ -1,43 +1,39 @@
-import React, { useContext } from 'react';
-import styled from 'styled-components';
+import React, { useContext } from "react";
+import styled from "styled-components";
 
-import Tile from '../molecules/Tile';
-import AppContext from '../../context';
+import Tile from "../molecules/Tile";
+import AppContext from "../../context";
 
 const StyledWrapper = styled.div`
-display: flex;
-justify-content: center;
-width: 500px;
+  display: flex;
+  justify-content: center;
+  width: 500px;
 `;
 
 const Rack = () => {
-    const { moves, actualMoveIndex, actualOptionIndex } = useContext(AppContext);
-    const move = moves[actualMoveIndex];
-    if (!move.choiceOptions[actualOptionIndex]) return null;
-    const freeLetters = move.choiceOptions[actualOptionIndex].freeLetters.split('');
+  const { moves, actualMoveIndex, actualOptionIndex } = useContext(AppContext);
+  const move = moves[actualMoveIndex];
+  if (!move.choiceOptions[actualOptionIndex]) return null;
+  const freeLetters = move.choiceOptions[actualOptionIndex].freeLetters.split(
+    ""
+  );
 
-    const checkAndRemoveLetter = el => {
-        const index = freeLetters.indexOf(el);
-        if (index !== -1) {
-            freeLetters.splice(index, 1);
-            return false;
-        }
-        return true
+  const checkAndRemoveLetter = (el) => {
+    const index = freeLetters.indexOf(el);
+    if (index !== -1) {
+      freeLetters.splice(index, 1);
+      return false;
     }
+    return true;
+  };
 
-    const tiles = move.letters.split('').map((el, i) => (
-        <Tile
-            key={i}
-            letter={el}
-            played={checkAndRemoveLetter(el)}
-        />
-    ))
+  const tiles = move.letters
+    .split("")
+    .map((el, i) => (
+      <Tile key={i} letter={el} played={checkAndRemoveLetter(el)} />
+    ));
 
-    return (
-        <StyledWrapper>
-            {tiles}
-        </StyledWrapper>
-    );
-}
+  return <StyledWrapper data-testid="rack">{tiles}</StyledWrapper>;
+};
 
 export default Rack;
---
+++ src/components/organisms/tests/Rack.test.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+import Rack from "../Rack";
+import { contextValues } from "../../../dummyContextValues";
+import Context from "../../../context";
+
+const renderRack = () => {
+  contextValues.actualMoveIndex = 1;
+  return render(
+    <Context.Provider value={contextValues}>
+      <Rack />
+    </Context.Provider>
+  );
+};
+
+describe("Rack", () => {
+  it("renders with correct styles", () => {
+    const { getByTestId } = renderRack();
+    expect(getByTestId("rack")).toHaveStyle("width: 500px");
+  });
+
+  it("return null if  choiceOptions not exist", () => {
+    contextValues.moves[1].choiceOptions[contextValues.actualOptionIndex] = "";
+    const { container } = renderRack();
+    expect(container).toBeEmptyDOMElement();
+  });
+});
