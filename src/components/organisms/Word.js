16d67bc40b6a5ac2c4b71eaa737761d2be835e07
mazpawel94 <mazpawel94@gmail.com>
2021-02-09 20:42:28 +0100

    fix bug in set color for new word on board

--- src/components/organisms/Word.js
+++ src/components/organisms/Word.js
@@ -2,32 +2,30 @@ import React from "react";
 
 import BoardTile from "../atoms/BoardTile";
 import {
-    betweenBracketsValidator,
-    setPosition,
+  betweenBracketsValidator,
+  setPosition,
 } from "../../services/gameService";
 
 const size = 570 / 15;
 
 const Word = ({ isNewMove, letters, coordinates }) => {
-    const { x, y, verticle } = setPosition(coordinates);
-    const isBetweenBrackets = betweenBracketsValidator(letters);
+  const { x, y, verticle } = setPosition(coordinates);
+  const betweenBracketsArray = betweenBracketsValidator(letters);
 
-    const lettersDivs = () =>
-        letters
-            .split("")
-            .filter(letter => !['(', ')'].includes(letter))
-            .map((letter, index) => (
-                <BoardTile
-                    x={verticle ? x * size : ((x + index) * size)}
-                    y={verticle ? ((y + index) * size) : y * size}
-                    letter={letter}
-                    transparent={isBetweenBrackets(index + 1)}
-                    newMove={isNewMove}
-                />
-            ));
-    return (
-        <>{lettersDivs()}</>
-    );
+  const lettersDivs = letters
+    .split("")
+    .filter((letter) => !["(", ")"].includes(letter))
+    .map((letter, index) => (
+      <BoardTile
+        key={index}
+        x={verticle ? x * size : (x + index) * size}
+        y={verticle ? (y + index) * size : y * size}
+        letter={letter}
+        transparent={betweenBracketsArray[index]}
+        newMove={isNewMove}
+      />
+    ));
+  return <>{lettersDivs}</>;
 };
 
 export default Word;
--- src/services/gameService.js
+++ src/services/gameService.js
@@ -5,59 +5,65 @@ import Word from "../components/organisms/Word";
 const exceptCoordinates = ["*xch", "xch"];
 
 export const isLossMove = (moves, moveIndex) =>
-    moves[moveIndex].pointsBefore === moves[moveIndex + 2].pointsBefore;
+  moves[moveIndex].pointsBefore === moves[moveIndex + 2].pointsBefore;
 
 const isExchange = (coordinates) =>
-    exceptCoordinates.some((el) => el === coordinates);
+  exceptCoordinates.some((el) => el === coordinates);
 
 export const findPlayedMove = (move) => {
-    return move?.choiceOptions?.find((opt) => opt.coordinates.includes("*"));
-}
+  return move?.choiceOptions?.find((opt) => opt.coordinates.includes("*"));
+};
 
 export const setPosition = (coordinates) => {
-    const coord = coordinates
-        .split("")
-        .filter((el) => el !== "*")
-        .join("");
-    if (coord.slice(-1) !== coord.slice(-1).toLowerCase())
-        //horizontal
-        return {
-            x: coord.slice(-1).charCodeAt() - 65,
-            y: coord.slice(0, -1) - 1,
-            verticle: false,
-        };
-    else
-        return {
-            x: coord[0].charCodeAt() - 65,
-            y: coord.slice(1) - 1,
-            verticle: true,
-        };
+  const coord = coordinates
+    .split("")
+    .filter((el) => el !== "*")
+    .join("");
+  if (coord.slice(-1) !== coord.slice(-1).toLowerCase())
+    //horizontal
+    return {
+      x: coord.slice(-1).charCodeAt() - 65,
+      y: coord.slice(0, -1) - 1,
+      verticle: false,
+    };
+  else
+    return {
+      x: coord[0].charCodeAt() - 65,
+      y: coord.slice(1) - 1,
+      verticle: true,
+    };
 };
 export const getCurrentMoves = (moves, actualMoveIndex) =>
-    moves.slice(0, actualMoveIndex);
+  moves.slice(0, actualMoveIndex);
 
 export const getCurrentWords = (moves, actualMoveIndex) =>
-    getCurrentMoves(moves, actualMoveIndex).map((move, index) => {
-        const { word, coordinates } = findPlayedMove(move);
-        if (isExchange(coordinates) || isLossMove(moves, index)) return null;
-        return <Word key={index} letters={word} coordinates={coordinates} />;
-    });
-
+  getCurrentMoves(moves, actualMoveIndex).map((move, index) => {
+    const { word, coordinates } = findPlayedMove(move);
+    if (isExchange(coordinates) || isLossMove(moves, index)) return null;
+    return <Word key={index} letters={word} coordinates={coordinates} />;
+  });
 
 export const isMoveWithWord = (move) =>
-    !exceptCoordinates.some((el) => el === move.coordinates);
+  !exceptCoordinates.some((el) => el === move.coordinates);
 
 export const betweenBracketsValidator = (letters) => {
-    const bracketsPairs = [];
-    const findPair = (i) => {
-        const start = letters.indexOf("(", i);
-        const end = letters.indexOf(")", i + 1);
-        if (end !== -1) {
-            bracketsPairs.push({ start, end });
-            findPair(end);
-        }
-    };
-    findPair(0);
-    return (index) =>
-        bracketsPairs.some((pair) => pair.start < index && pair.end > index);
+  const bracketsPairs = [];
+  const findPair = (i) => {
+    const start = letters.indexOf("(", i);
+    const end = letters.indexOf(")", i + 1);
+    if (end !== -1) {
+      bracketsPairs.push({ start, end });
+      findPair(end);
+    }
+  };
+  findPair(0);
+  const isBetween = (index) =>
+    bracketsPairs.some((pair) => pair.start < index && pair.end > index);
+  const booleanArray = letters
+    .split("")
+    .map((letter, i) =>
+      ["(", ")"].includes(letter) ? undefined : isBetween(i)
+    )
+    .filter((el) => el !== undefined);
+  return booleanArray;
 };
