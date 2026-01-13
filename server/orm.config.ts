3002c9814c4a0777f50ef40e15212fe767f34e5f
mazpawel94 <mazpawel94@gmail.com>
2026-01-13 20:19:39 +0100

    fix orm config

--- server/orm.config.ts
+++ server/orm.config.ts
@@ -3,11 +3,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const config: TypeOrmModuleOptions = {
   type: 'postgres',
   url: process.env.DB_GCG_URL,
-  host: process.env.DB_GCG_HOST,
-  port: 5432,
-  username: process.env.DB_GCG_USERNAME,
-  password: process.env.DB_GCG_PASSWORD,
-  database: 'postgres',
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
   entities: ['dist/**/*.entity{.ts,.js}'],
-  synchronize: true,
+  synchronize: process.env.NODE_ENV !== 'production',
 };
