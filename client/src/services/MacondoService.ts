5817e08bcbfe75bd3a66e6598c19d17c65821957
mazpawel94 <mazpawel94@gmail.com>
2026-01-14 13:36:02 +0100

    log

--- client/src/services/MacondoService.ts
+++ client/src/services/MacondoService.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 class MacondoService {
   static async analyzeMove(rack: string, playedMove: string, withFullAnalysis: boolean) {
+    console.log({rack, playedMove, withFullAnalysis});
     const res = await axios({
       method: 'post',
       url: withFullAnalysis ? `/api/analyze` : `/api/analyze/simple`,
